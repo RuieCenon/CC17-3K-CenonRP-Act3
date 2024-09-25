b6153178f89b221da822b42e9f3363a183403a0e
Ruie842 <164741746+RuieCenon@users.noreply.github.com>
2024-09-25 09:51:17 +0800

    Dice roller

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.rollButton)
+        val imageView = findViewById<ImageView>(/* id = */ R.id.imageView)
+
+
+        button.setOnClickListener {
+            val n = (1.rangeTo(6)).random()
+
+            imageView.setImageResource(when (n) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6 -> R.drawable.dice_6
+                else -> throw Exception("n must be between 1 and 6 only")
+            })
+        }
+    }
+
+}
